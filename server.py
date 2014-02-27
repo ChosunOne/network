49d2b33c9c250bd86cb198d6fc8b08f46b162f97
ChosunOne <koreanhalo@gmail.com>
2014-02-27 15:44:30 -0600

    Integrated server object
    For some reason server.py wasn't included in the last commit.

--- server.py
+++ server.py
@@ -3,38 +3,11 @@ import threading
 from queue import Queue
 from networkLibrary import *
 
+port = int(input('Please indicate a port for the server'))
+
 print('Setting up server')
 
-s = socket.socket()
-host = socket.gethostname()
-port = 12345
-q = Queue()
-que = []
-que.append(q)
-
-MAXUSERS = 4
-
-s.bind((host, port))
-
-print(host, 'set up on port', port)
-
-s.listen(5)
-
-print('Waiting for connections')
-
-while True:
-    (c, (ip, port)) = s.accept()
-    if c:
-        
-        user = client(ip, port, c)
-        que[0].put(user)
-        
-        t = threading.Thread(target=clientWorker, args=que)
-        t.daemon = True
-        t.start()
-
-q.join()
-
-    
+s = server(port)
+s.start()
 
     
